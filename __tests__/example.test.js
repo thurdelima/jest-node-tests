bd471c149adc934cc37f11f9daab6c326c09d507
Arthur Pires de Lima <thurdelima@MacBook-de-Arthur.local>
2020-04-25 12:33:28 -0300

    first commit

---
+++ __tests__/example.test.js
@@ -0,0 +1,11 @@
+const expectExport = require("expect");
+
+function soma(a, b) {
+  return a + b;
+}
+
+test('if I call soma function with 4 and 5 it should return 9', () => {
+  const result = soma(4, 5);
+
+  expect(result).toBe(9);
+});
